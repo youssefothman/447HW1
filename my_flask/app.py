1e79a97cbfde495cbc000d62f9f4395c5519de48
Youssef Othman <yothman1@umbc.edu>
2022-02-27 23:33:54 -0500

    getting the flask

--- my_flask/app.py
+++ my_flask/app.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imports.db'
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    points = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    people = Person.query.order_by(Person.id)
+    return render_template("index.html", people=people)
 
-@app.route('/add')
+@app.route('/add', methods=['POST', 'GET'])
 def add():
-    return render_template("add.html")
+
+    if request.method == "POST":
+        person_name = request.form['name']
+        person_id = request.form['id']
+        person_points = request.form['points']
+        new_person = Person(id=person_id, name=person_name, points=person_points)
+
+        try:
+            db.session.add(new_person)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an error adding that person."
+
+    else:
+        people = Person.query.order_by(Person.id)
+        return render_template("add.html", people=people)
 
 @app.route('/update')
 def update():
