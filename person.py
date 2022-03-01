24ea783969bb48368b649e2a0cc01fc056e88a03
youssefothman <78071061+youssefothman@users.noreply.github.com>
2022-03-01 17:28:31 -0500

    Delete person.py

--- person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, id, points):
-        self.name = name
-        self.id = id
-        self.points = points
-
-    def __repr__(self):
-        return "Name"+self.name+". ID: "+self.id+". Points: "+self.points+"."
